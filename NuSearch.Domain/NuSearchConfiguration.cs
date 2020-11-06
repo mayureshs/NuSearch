669d7a5236241ef1384171236369a070d503efe6
Mayuresh Sawardekar <msawardekar@technologypartners.net>
2020-11-05 19:06:45 -0600

    Add project files.

---
+++ NuSearch.Domain/NuSearchConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Nest;
+using NuSearch.Domain.Model;
+
+namespace NuSearch.Domain
+{
+	public static class NuSearchConfiguration
+    {
+        public static ElasticClient GetClient() => new ElasticClient(_connectionSettings);
+
+        static NuSearchConfiguration()
+        {
+            _connectionSettings = new ConnectionSettings(CreateUri(9200)).DefaultIndex("nusearch")
+                .DefaultMappingFor<Package>(i => i
+                    .TypeName("package")
+                    .IndexName("nusearch")
+                );
+        }
+
+        private static readonly ConnectionSettings _connectionSettings;
+
+        public static string LiveIndexAlias => "nusearch";
+
+        public static string OldIndexAlias => "nusearch-old";
+
+        public static Uri CreateUri(int port)
+        {
+            var host = Process.GetProcessesByName("fiddler").Any()
+                ? "ipv4.fiddler"
+                : "localhost";
+
+            return new Uri($"http://{host}:{port}");
+        }
+
+        public static string CreateIndexName() => $"{LiveIndexAlias}"; //-{DateTime.UtcNow:dd-MM-yyyy-HH-mm-ss}
+
+        public static string PackagePath =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? @"C:\code\projects\NuSearch\Data\nuget-data" : "/nuget-data";
+    }
+}
